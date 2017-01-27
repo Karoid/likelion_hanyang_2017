cae05fa7027a04519a0d67729ae5cb1018824144
Karoid <shj5508@naver.com>
2017-01-27 23:55:20 +0900

    mobile navigation menu made

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,6 @@
 class AdminController < ApplicationController
   layout "admin_application"
   def index
-    #flash[:success ] = "Success Flash Message: Welcome to GentellelaOnRails"
-    #other alternatives are
-    # flash[:warn ] = "Israel don't quite like warnings"
-    #flash[:danger ] = "Naomi let the dog out!"
     authorize! :read, Category.where(route: "admin").take
   end
   def show_category
